4003873e5909dfd5698946f63177e9d177b6c7f5
alertjjm <sdr2936@gmail.com>
2020-08-18 16:22:38 +0900

    deleted unnecessaries

--- httphdr.h
+++ httphdr.h
@@ -1,12 +1,10 @@
 #pragma once
 #include <cstdint>
 #include <string>
-#include <map>
 using namespace std;
 
 #pragma pack(push, 1)
 struct HttpHdr final {
-	map<string,string> comp;
     HttpHdr(string packet, int hlen);
     string getHost();
     int httplen;
